fc63dd23651a36760c4b60b31d1e5435f7f66dc3
uwaiseibna <uwaiseibna03@gmail.com>
2023-10-27 14:17:50 -0400

    change-port

--- app.py
+++ app.py
@@ -104,5 +104,5 @@ def download(filename):
     return redirect(gcs_url)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
