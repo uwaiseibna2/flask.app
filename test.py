b782e3090ac5ea9ba452c31bfe2b654dafe961d1
uwaiseibna <uwaiseibna03@gmail.com>
2023-11-24 14:12:33 -0500

    update-test

--- test.py
+++ test.py
@@ -1,19 +1,39 @@
 import unittest
-from app import app
+from app import app, db, User
+from flask_login import current_user
 
-class FlaskAppTest(unittest.TestCase):
-
+class TestFlaskApp(unittest.TestCase):
     def setUp(self):
+        self.app_context = app.app_context()
+        self.app_context.push()
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        db.create_all()
+
         self.app = app.test_client()
-        self.app.testing = True
 
-    def test_index_page(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Login', response.data)
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
 
-    # Add more test cases for your application
+    def test_register_and_login(self):
+        with app.test_request_context():
+            # Register a new user
+            response = self.app.post('/register', data=dict(
+                username='testuser',
+                password='testpassword'
+            ), follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
 
+            # Log in with the registered user
+            response = self.app.post('/login', data=dict(
+                username='testuser',
+                password='testpassword'
+            ), follow_redirects=True)
+            self.assertIn(b'Image Gallery', response.data)
+            self.assertEqual(current_user.username, 'testuser')
+
+    
 if __name__ == '__main__':
     unittest.main()
-
